0e74a8faafe0149113e7c1838f4a40b1b42151a0
Jason <128120655+Jasonfan06@users.noreply.github.com>
2025-11-21 17:31:49 -0800

    a mesh grid

--- variate_alpha.py
+++ variate_alpha.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from fipy import CellVariable, Grid2D, DiffusionTerm, TransientTerm
+
+# a mesh grid
+nx = ny = 100
+L = 1.0
+dx = dy = L / nx
+mesh = Grid2D(nx=nx, ny=ny, dx=dx, dy=dy)
