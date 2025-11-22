87c7b26b413f2b0244d7961d70864faf7c12123d
Jason <128120655+Jasonfan06@users.noreply.github.com>
2025-11-21 17:34:14 -0800

    some minor changes to mesh grid
    Set a mesh grid with varying alpha from 1e-6 to 1.

--- variate_alpha.py
+++ variate_alpha.py
@@ -7,3 +7,11 @@ nx = ny = 100
 L = 1.0
 dx = dy = L / nx
 mesh = Grid2D(nx=nx, ny=ny, dx=dx, dy=dy)
+
+x, y = mesh.cellCenters  
+
+D = CellVariable(mesh=mesh, value=1e-6 + (1 - 1e-6) * x)
+
+'''
+so the thing above sets a mesh grid with varying alpha (linear increase from 1e-6 to 1 (linear increase)
+'''
